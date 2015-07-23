7cf2536629514d4b51e6d27dd6cb9ac03c961fb2
RandyyDSH <Randyy@gmail.com>
2015-07-23 19:23:38 +0300

    Частичная реализация класса игры
    Нужно
    - реализовать ходы;
    - Сохранение игры;
    - вохможность запускать паралельно?

--- Model/chessgame.cpp
+++ Model/chessgame.cpp
@@ -1,7 +1,47 @@
 #include "chessgame.h"
 
-chessGame::chessGame()
+chessGame::chessGame() : player1(nullptr), player2(nullptr)
 {
 
+}
+
+chessGame::chessGame(chessPlayer *p1, chessPlayer *p2) : player1(p1), player2(p2), lIsGameInProgress(true)
+{
+    p1->setIsWhite(true);
+    p2->setIsWhite(false);
+}
+
+chessGame::~chessPlayer()
+{
+    delete player1;
+    player1 = nullptr;
+    delete player2;
+    player2 = nullptr;
+}
+
+void chessGame::newGame()
+{
+    state.init();
+
+    lIsGameInProgress = false;
+    player1->newGame();
+    player2->newGame();
+
+    history.clear();
+    redo.clear();
+
+}
+
+void chessGame::loadGame(const chessGameState &cgs)
+{
+    player1->loadGame(cgs);
+    player2->loadGame(cgs);
+}
+
+void chessGame::startGame()
+{
+    player1->startGame();
+    player2->startGame();
+    lIsGameInProgress = true;
 }
 
--- Model/chessgame.h
+++ Model/chessgame.h
@@ -1,11 +1,56 @@
 #ifndef CHESSGAME_H
 #define CHESSGAME_H
 
+//Самописные классы
+#include "chessplayer.h"
 
+/*!
+ * \brief The chessGame class - класс партии
+ */
 class chessGame
 {
 public:
     chessGame();
+    /*!
+     * \brief chessGame - конструктор для инициализации парой игроков
+     * \param p1 - белый игрок
+     * \param p2 - черный игрок
+     */
+    chessGame(chessPlayer *p1, chessPlayer *p2);
+    /*!
+     * \brief ~chessGame корректно разрушает объект
+     */
+    ~chessGame();
+    /*!
+     * \brief newGame - запускает новую игру
+     */
+    void newGame();
+    /*!
+     * \brief loadGame - загружает игру
+     * \param cgs - состояние игры
+     */
+    void loadGame(const chessGameState &cgs);
+    /*!
+     * \brief startGame - начинает новую партию
+     */
+    void startGame();
+    /*!
+     * \brief getPlaer1 - возвращает белого игрока
+     */
+    inline chessPlayer* getPlaer1() const {  return player1; }
+    /*!
+     * \brief getPlaer2 - возвращает белого игрока
+     */
+    inline chessPlayer* getPlaer2() const {  return player2; }
+private:
+    chessPlayer *player1;
+    chessPlayer *player2;
+    chessGameState state;
+
+    QList<chessGameState> history;
+    QList<chessGameState> redo;
+
+    bool lIsGameInProgress;
 };
 
 #endif // CHESSGAME_H
